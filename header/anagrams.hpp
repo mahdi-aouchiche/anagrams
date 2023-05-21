bc98f8c9a3ec7de452a9d7b022ffed1f0513e05c
Mahdi A <mahdiaouchiche2022@gmail.com>
2023-05-20 19:53:53 -0700

    Header files

---
+++ header/anagrams.hpp
@@ -0,0 +1,14 @@
+#ifndef __ANAGRAMS_HPP__
+#define __ANAGRAMS_HPP__
+
+#include <iostream>	// cout
+#include <string>	// string
+#include <vector>	// vecotr
+#include <algorithm>    // std::sort
+
+using namespace std;
+
+int string_value(string str);
+void output_anagrams(vector<string>);
+
+#endif // __ANAGRAMS_HPP__
